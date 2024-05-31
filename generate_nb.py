f96fcf1302a19c2ab2b003d06b610a4920dcb79f
Nicolas <nicolas.a.billy@gmail.com>
2024-05-31 14:31:24 +0200

    initial commit with official repo of stylegan2-Ada-pytorch

---
+++ generate_nb.py
@@ -0,0 +1,75 @@
+import os
+import re
+from typing import List, Optional
+
+import dnnlib
+import numpy as np
+import PIL.Image
+import torch
+
+import legacy
+
+def num_range(s: str) -> List[int]:
+    '''Accept either a comma separated list of numbers 'a,b,c' or a range 'a-c' and return as a list of ints.'''
+
+    range_re = re.compile(r'^(\d+)-(\d+)$')
+    m = range_re.match(s)
+    if m:
+        return list(range(int(m.group(1)), int(m.group(2))+1))
+    vals = s.split(',')
+    return [int(x) for x in vals]
+
+def generate_images(
+    network_pkl: str,
+    seeds: Optional[List[int]],
+    truncation_psi: float,
+    noise_mode: str,
+    outdir: str,
+    class_idx: Optional[int] = None,
+    projected_w: Optional[str] = None,
+    show: bool = False
+):
+    print('Loading networks from "%s"...' % network_pkl)
+    device = torch.device('cpu')
+    with dnnlib.util.open_url(network_pkl) as f:
+        G = legacy.load_network_pkl(f)['G_ema'].to(device)
+
+    os.makedirs(outdir, exist_ok=True)
+
+    if projected_w is not None:
+        if seeds is not None:
+            print ('warn: --seeds is ignored when using --projected-w')
+        print(f'Generating images from projected W "{projected_w}"')
+        ws = np.load(projected_w)['w']
+        ws = torch.tensor(ws, device=device)
+        assert ws.shape[1:] == (G.num_ws, G.w_dim)
+        for idx, w in enumerate(ws):
+            img = G.synthesis(w.unsqueeze(0), noise_mode=noise_mode)
+            img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+            img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/proj{idx:02d}.png')
+        return
+
+    if seeds is None:
+        print('Error: --seeds option is required when not using --projected-w')
+        return
+
+    label = torch.zeros([1, G.c_dim], device=device)
+    if G.c_dim != 0:
+        if class_idx is None:
+            print('Error: Must specify class label with --class when using a conditional network')
+            return
+        label[:, class_idx] = 1
+    else:
+        if class_idx is not None:
+            print ('warn: --class=lbl ignored when running on an unconditional network')
+
+    for seed_idx, seed in enumerate(seeds):
+        print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+        img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+        img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB')
+        if show:
+            return img
+        else:
+            img.save(f'{outdir}/seed{seed:04d}.png')
