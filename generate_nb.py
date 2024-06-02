ed1a9f1e347987361998caa94eb0a58da55ee989
Nicolas Billy <billy-22@gpu2.enst.fr>
2024-06-02 23:59:52 +0200

    modifications sur le gpu

--- generate_nb.py
+++ generate_nb.py
@@ -30,6 +30,31 @@ def generate_images(
     show: bool = False,
     device: str = 'cpu'
 ):
+    """Generate images using pretrained network pickle.
+
+    Examples:
+
+    \b
+    # Generate curated MetFaces images without truncation (Fig.10 left)
+    python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+
+    \b
+    # Generate uncurated MetFaces images with truncation (Fig.12 upper left)
+    python generate.py --outdir=out --trunc=0.7 --seeds=600-605 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+
+    \b
+    # Generate class conditional CIFAR-10 images (Fig.17 left, Car)
+    python generate.py --outdir=out --seeds=0-35 --class=1 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
+
+    \b
+    # Render an image from projected W
+    python generate.py --outdir=out --projected_w=projected_w.npz \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+    """
+    
     print('Loading networks from "%s"...' % network_pkl)
     device = torch.device(device)
     with dnnlib.util.open_url(network_pkl) as f:
